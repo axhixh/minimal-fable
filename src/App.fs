5774e277af523ed8fb225e0546da44499aa9b9db
Ashish Shrestha <>
2019-07-11 10:00:44 -0400

    ran code formatter in vscode

--- src/App.fs
+++ src/App.fs
@@ -3,8 +3,7 @@ module HelloApp
 open Browser
 
 let Greetings = "Hello, Fable!"
-
 let content = Dom.document.getElementById "content"
-content.innerHTML <- (sprintf "<h1>%s</h1>" Greetings)
 
+content.innerHTML <- (sprintf "<h1>%s</h1>" Greetings)
 Dom.console.log Greetings
