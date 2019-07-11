cc6669aef203bb596b31db7c7bbf32a2019f324a
Ashish Shrestha <>
2019-07-11 09:55:22 -0400

    changed to use Fable 3

--- src/App.fs
+++ src/App.fs
@@ -1,10 +1,10 @@
 module HelloApp
 
-open Fable.Import
+open Browser
 
 let Greetings = "Hello, Fable!"
 
-let content = Browser.document.getElementById "content"
+let content = Dom.document.getElementById "content"
 content.innerHTML <- (sprintf "<h1>%s</h1>" Greetings)
 
-Browser.window.console.log Greetings
+Dom.console.log Greetings
